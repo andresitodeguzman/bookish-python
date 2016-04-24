eae816df995c75ace160dcc5c609268fcf7654a8
Andresito de Guzman <deguzman.andresito@gmail.com>
2016-04-23 19:37:28 -0500

    Improved spacing and end message

--- initial/ask.py
+++ initial/ask.py
@@ -1,19 +1,19 @@
 #Ask how many f*cks do I give?
 
-progtitle = "Sample Code\n"
-progspace = "============="
+progtitle = "Sample Code"
+progspace = "=============\n"
 
 #Make things quite pretty
 print progtitle
 print progspace
 
 #Main part
-askfuck = input("How many fucks do I give? ")
+askfuck = input("How many f*cks do I give? ")
 askfuck = int(askfuck)
 if askfuck > 0:
-    print ("Think again bitch!")
+    print ("Think again b*tch!")
 else:
     print ("Good!")
 
 #End
-print "K dot"
+print "Kbye *smh*"
