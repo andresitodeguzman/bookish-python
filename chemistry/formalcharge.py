f372445fc1b23228a5cccd927ae3437d0ed45f51
Andresito de Guzman <deguzman.andresito@gmail.com>
2017-03-07 20:47:38 +0800

    Improved Formal Charge Calculator

--- chemistry/formalcharge.py
+++ chemistry/formalcharge.py
@@ -1,12 +1,24 @@
+#
+#   Andresito de Guzman
+#   Formal Charge Calculator
+#   Chemistry
+#   2017
+#
+
 def formalCharge(valence, nonbonding, bonding):
     v = int(valence)
     n = int(nonbonding)
     b = int(bonding)
     compute = int(v-n-((1/2)*b))
     return compute
-
-valence = input("Valence Electrons: ")
-nonbonding = input("Non-bonding Electrons: ")
-bonding = input("Bonding Electrons: ")
-solve = formalCharge(valence, nonbonding, bonding)
-print(solve)
+while True:
+    try:
+        valence = input("Valence Electrons: ")
+        nonbonding = input("Non-bonding Electrons: ")
+        bonding = input("Bonding Electrons: ")
+        solve = formalCharge(valence, nonbonding, bonding)
+        print(str(solve) + "\n")
+    except ValueError:
+        print("Error in your input")
+        pass
+        
